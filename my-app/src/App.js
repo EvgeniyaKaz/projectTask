1973b31e1eccfcf41cc2bfa3dbe91a815fe69c77
Zhenya <kerrigan.vig56@gmail.com>
2024-12-09 18:50:04 +0300

    add js-file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,43 @@
-import logo from "./logo.svg"; /*императивный*/
-import "./App.css"; /*императивный*/
+import logo from "./logo.svg";
+import "./App.css";
+import { createElement } from "react";
 
-function App() /*императивный*/ {
+function App() {
 	let date = new Date().getFullYear().toString();
-	return (
-		/*все html теги это декларативный стиль, так как мы запрашиваем нужные нам элементы*/
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{date}</p>
-			</header>
-		</div>
+	return /*#__PURE__*/ createElement(
+		"div",
+		{
+			className: "App",
+		},
+		/*#__PURE__*/ createElement(
+			"header",
+			{
+				className: "App-header",
+			},
+			/*#__PURE__*/ createElement("img", {
+				src: logo,
+				className: "App-logo",
+				alt: "logo",
+			}),
+			/*#__PURE__*/ createElement(
+				"p",
+				null,
+				"Edit ",
+				/*#__PURE__*/ createElement("code", null, "src/App.js"),
+				" and save to reload."
+			),
+			/*#__PURE__*/ createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			/*#__PURE__*/ createElement("p", null, date)
+		)
 	);
 }
 export default App;
